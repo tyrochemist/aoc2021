169899e649fa4ddf2f74b3f4c79e46fa15681d71
Tyler P Roche <tyler.roche@gmail.com>
2021-12-04 03:04:52 -0500

    adds 2-1

---
+++ day2/aoc2-1.py
@@ -0,0 +1,23 @@
+from numpy import genfromtxt
+
+# Import instructions as strings
+instructions = genfromtxt('input.csv')
+x = 0
+y = 0
+product = 0
+# Iterate through instructions
+for instruction in instructions:
+    # Split string into command and value
+    command = instruction.split()[0]
+    units = instruction.split[1]
+    # Check what command is and increment by value
+    if command == 'forward':
+        x += int(units)
+    if command == 'down':
+        y += int(units)
+    if command == 'up':
+        y -= int(units)
+# Multiply horizontal distance by depth
+product = x*y
+print(f'Your horizontal distance is {x} and your depth is {y}')
+print(f'The product of the two is {product}')
